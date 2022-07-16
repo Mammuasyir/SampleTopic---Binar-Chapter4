2b2ca00c9d476a6166bfad5a3323fdca95c10bc3
Mammuasyir <mammuasyir0101@gmail.com>
2022-07-16 13:25:04 +0800

    modify

---
+++ app/src/main/java/com/humam/chapter4/ChallengeSabtuActivity.kt
@@ -0,0 +1,11 @@
+package com.humam.chapter4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChallengeSabtuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_challenge_sabtu)
+    }
+}
